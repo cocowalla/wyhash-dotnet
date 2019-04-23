909ac30ec4771028a91def0dc735eec582695814
cocowalla <800977+cocowalla@users.noreply.github.com>
2019-04-23 13:20:26 +0100

    Improve 64x64 multiplication tests by comparing results against System.Numerics.BigInteger

--- test/UnitTests/WyCoreTests.cs
+++ test/UnitTests/WyCoreTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Numerics;
 using Shouldly;
 using Xunit;
 
@@ -5,17 +7,24 @@ namespace WyHash.UnitTests
 {
     public class WyCoreTests
     {
-        [InlineData(2, 2, 0, 4)]
-        [InlineData(2_147_483_647, 100, 0, 214_748_364_700)]
-        [InlineData(2_147_483_647, 2_147_483_647, 0, 4_611_686_014_132_420_609)]
-        [InlineData(2_147_483_647, 100_000_000_000, 11, 11_834_179_889_194_932_224)]
-        [InlineData(2_147_483_647, 100_000_000_000_000, 11_641, 9_816_937_947_109_638_144)]
+        [InlineData(2, 2)]
+        [InlineData(2_147_483_647, 100)]
+        [InlineData(2_147_483_647, 2_147_483_647)]
+        [InlineData(2_147_483_647, 100_000_000_000)]
+        [InlineData(2_147_483_647, 100_000_000_000_000)]
+        [InlineData(UInt64.MaxValue, UInt64.MaxValue)]
+        [InlineData(UInt64.MaxValue - 1, UInt64.MaxValue)]
+        [InlineData(18446744073709551610, 18446744073709551610)]
         [Theory]
-        public void Should_multiply_64_bit_integers(ulong x, ulong y, ulong expectedHi, ulong expectedLo)
+        public void Should_multiply_64_bit_integers(ulong x, ulong y)
         {
+            var expectedProduct = BigInteger.Multiply(x, y);
+            var expectedHi = (ulong)(expectedProduct >> 64);
+            var expectedLo = (ulong)(expectedProduct & 0xfffffffffffffffful);
+
             // ReSharper disable once UseDeconstruction
             var result = WyCore.Multiply64(x, y);
-            
+
             result.Hi.ShouldBe(expectedHi);
             result.Lo.ShouldBe(expectedLo);
         }
